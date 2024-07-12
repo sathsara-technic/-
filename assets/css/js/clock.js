150daed980610321c7ad87822900e61134cf40fa
Sathsara Modz <145262720+sathsara-technic@users.noreply.github.com>
2024-07-12 15:45:02 +0530

    Create clock.js

---
+++ assets/css/js/clock.js
@@ -0,0 +1,35 @@
+body {
+
+width: 100%;
+
+height: 100vh;
+
+display: flex;
+
+justify-content: center;
+
+align-items: center;
+
+background-color: #000;
+
+font-family: sans-serif;
+
+}
+
+.card {
+
+padding: 4px 30px;
+
+align-items: center;
+
+box-shadow: 0px 0px 8px #03AED2;
+
+border-radius: 8px;
+
+ }
+
+.card h1 {
+
+color: #03AED2;
+
+}
